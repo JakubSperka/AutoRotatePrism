50093f9fb7d5f6eb3a9a7d926644ef5bc85f1c35
JakubSperka <131759253+JakubSperka@users.noreply.github.com>
2024-03-19 15:02:54 +0100

    Added some comments

--- import_data.py
+++ import_data.py
@@ -2,11 +2,11 @@
 Function to import data from txt files used as command in tkinter button
 """
 
-#Import modules and libraries
+# Import modules and libraries
 from tkinter import filedialog
 import pandas as pd
 
-network_points = None
+network_points = pd.DataFrame()
 
 
 def import_network():
@@ -15,30 +15,16 @@ def import_network():
     if file_path:
         try:
             global network_points
-            network_points = pd.read_csv(file_path, names=["ID", "X", "Y", "H", "Code"], delimiter=',')
+            imported_network_points = pd.read_csv(file_path, names=["ID", "X", "Y", "H", "Code"], delimiter=',')
 
+            network_points = pd.concat([imported_network_points, network_points])
             # Print or use the data as needed
+
             print("Imported Network points:")
             print(network_points)
 
+
             return network_points
-
-        except Exception as e:
-            print(f"Error reading the file: {e}")
-    else:
-        print("No file selected.")
-
-
-def import_temp():
-    file_path = filedialog.askopenfilename(title="Select Point Data File", filetypes=[("Text files", "*.txt")])
-
-    if file_path:
-        try:
-            temp_points = pd.read_csv(file_path, names=["ID", "X", "Y", "H", "Code"], delimiter='\t')
-
-            # Print or use the data as needed
-            print("Imported Temporary points:")
-            print(temp_points)
 
         except Exception as e:
             print(f"Error reading the file: {e}")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Main
 
 import tkinter as tk
+from tkinter import ttk
 from import_data import import_network
+from import_data import network_points
 
 root = tk.Tk()
 root.title("ARP Control Panel v.1.0.0")
@@ -11,5 +13,21 @@ frame_network_points.pack()
 
 import_button = tk.Button(frame_network_points, text="Import Point Data", command=import_network)
 import_button.pack(pady=20)
+
+# Create a Treeview widget
+tree = ttk.Treeview(frame_network_points, columns=["ID", "X", "Y", "H", "Code"], show="headings", selectmode="browse")
+
+# Add columns to the Treeview
+for col in ["ID", "X", "Y", "H", "Code"]:
+    tree.heading(col, text=col)
+    tree.column(col, anchor="center")
+
+# Insert data into the Treeview
+for index, row in network_points.iterrows():
+    tree.insert("", "end", values=(row["ID"], row["X"], row["Y"], row["H"], row["Code"]))
+
+
+# Pack the Treeview onto the window
+tree.pack(pady=20)
 
 root.mainloop()
