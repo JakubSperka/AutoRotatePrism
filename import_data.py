0c5c56af2b915bc0fcfc975675b3051706d55883
JakubSperka <131759253+JakubSperka@users.noreply.github.com>
2024-03-25 22:58:06 +0100

    Added Comments, Scrollbars and Equipment Tab

--- import_data.py
+++ import_data.py
@@ -1,21 +1,24 @@
 """
-Function to import data from txt files used as command in tkinter button
+Functions to import data from txt files used as command in tkinter button
 """
 
 # Import modules and libraries
 from tkinter import filedialog
 import pandas as pd
 
-# Define dataframes for defining global variables
+# Define variables as dataframes for defining global variables later in code
 network_points = pd.DataFrame()
 
 
+# Function to import geodetic network points
 def import_network(tree_name):
     # Get filepath from opened file explorer dialog
     file_path = filedialog.askopenfilename(title="Select Point Data File", filetypes=[("Text files", "*.txt")])
 
+    # Sequence after retrieving the filepath
     if file_path:
         try:
+            # Defining global variable and reading, data from .txt file and inserting into dataframe
             global network_points
             network_points = pd.read_csv(file_path, names=["ID", "X", "Y", "H", "Code"], delimiter=',')
 
@@ -25,10 +28,14 @@ def import_network(tree_name):
             # Clear previous data in the Treeview
             tree_name.delete(*tree_name.get_children())
 
+            # Insert data into Treeview
             for index, row in network_points.iterrows():
                 tree_name.insert("", "end", values=(row["ID"], row["X"], row["Y"], row["H"], row["Code"]))
 
+        # Error message when invalid file is selected
         except Exception as e:
             print(f"Error reading the file: {e}")
+
+    # Error message when no file is selected
     else:
         print("No file selected.")
--- main.py
+++ main.py
@@ -11,14 +11,28 @@ from import_data import import_network
 root = tk.Tk()
 root.title("ARP Control Panel v.1.0.0")
 
-# Create a frame from imported network points
+"""
+Commands for Network points tab
+"""
+# Create a frame for imported network points
 frame_network_points = tk.LabelFrame(root, text="Imported network points", padx=10, pady=10)
 frame_network_points.pack()
 
-# Create a Treeview widget
-network_tree = ttk.Treeview(frame_network_points, columns=["ID", "X", "Y", "H", "Code"],
-                            show="headings", selectmode="browse")
-network_tree.pack(pady=10)
+# Create a frame for network points Treeview
+treeview_frame_network_points = tk.Frame(frame_network_points)
+treeview_frame_network_points.pack()
+
+# Create a scrollbar for network points Treeview
+network_tree_scrollbar = tk.Scrollbar(treeview_frame_network_points)
+network_tree_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# Create a Treeview widget for imported network points
+network_tree = ttk.Treeview(treeview_frame_network_points, columns=["ID", "X", "Y", "H", "Code"],
+                            show="headings", selectmode="browse", yscrollcommand=network_tree_scrollbar.set)
+network_tree.pack()
+
+# Config scrollbar for network points Treeview
+network_tree_scrollbar.config(command=network_tree.yview)
 
 # Add columns to the Treeview for imported network points
 for col in ["ID", "X", "Y", "H", "Code"]:
@@ -28,6 +42,36 @@ for col in ["ID", "X", "Y", "H", "Code"]:
 # Add button for importing network points
 import_button = tk.Button(frame_network_points, text="Import Point Data", command=lambda: import_network(network_tree))
 import_button.pack(pady=5)
+
+"""
+Commands for Equipment tab
+"""
+
+# Create a frame for equipment tab
+frame_equipment = tk.LabelFrame(root, text="Equipment", padx=10, pady=10)
+frame_equipment.pack()
+
+# Create a frame for equipment Treeview
+treeview_frame_equipment = tk.Frame(frame_equipment)
+treeview_frame_equipment.pack()
+
+# Create a scrollbar for equipment Treeview
+equipment_tree_scrollbar = tk.Scrollbar(treeview_frame_equipment)
+equipment_tree_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# Create a Treeview widget for equipment
+equipment_tree = ttk.Treeview(treeview_frame_equipment,
+                              columns=["ARP_ID", "Auth_token", "Prism_height", "Prism_constant"],
+                              show="headings", selectmode="browse", yscrollcommand=equipment_tree_scrollbar.set)
+equipment_tree.pack()
+
+# Config scrollbar for equipment Treeview
+equipment_tree_scrollbar.config(command=equipment_tree.yview)
+
+# Add columns to the Treeview for equipment
+for col in ["ARP_ID", "Auth_token", "Prism_height", "Prism_constant"]:
+    equipment_tree.heading(col, text=col, anchor="w")
+    equipment_tree.column(col, anchor="w", width=125)
 
 # Initialize mainloop for the root window
 root.mainloop()
