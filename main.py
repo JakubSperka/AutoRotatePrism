8972012e07dfb885b87a9c62141352619a7ad228
JakubSperka <131759253+JakubSperka@users.noreply.github.com>
2024-03-25 16:40:56 +0100

    Added some comments

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 # Main
 
+# Imported packages
 import tkinter as tk
 from tkinter import ttk
 from import_data import import_network
 
+# Set root GUI window
 root = tk.Tk()
 root.title("ARP Control Panel v.1.0.0")
 
-# Create a frame
+# Create a frame from imported network points
 frame_network_points = tk.LabelFrame(root, text="Imported network points", padx=10, pady=10)
 frame_network_points.pack()
 
 # Create a Treeview widget
-tree = ttk.Treeview(frame_network_points, columns=["ID", "X", "Y", "H", "Code"], show="headings", selectmode="browse")
-tree.pack(pady=10)
+network_tree = ttk.Treeview(frame_network_points, columns=["ID", "X", "Y", "H", "Code"],
+                            show="headings", selectmode="browse")
+network_tree.pack(pady=10)
 
-# Add columns to the Treeview
+# Add columns to the Treeview for imported network points
 for col in ["ID", "X", "Y", "H", "Code"]:
-    tree.heading(col, text=col, anchor="w")
-    tree.column(col, anchor="w", width=100)
+    network_tree.heading(col, text=col, anchor="w")
+    network_tree.column(col, anchor="w", width=100)
 
-import_button = tk.Button(frame_network_points, text="Import Point Data", command=lambda: import_network(tree))
+import_button = tk.Button(frame_network_points, text="Import Point Data", command=lambda: import_network(network_tree))
 import_button.pack(pady=5)
 
 root.mainloop()
